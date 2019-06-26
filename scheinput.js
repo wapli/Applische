569a605ea41740ce01a250adb78ec9a2fb7c77d3
kinntaro <moneybook0409@outlook.jp>
2019-06-26 19:45:19 +0900

    no message

--- scheinput.js
+++ scheinput.js
@@ -1,16 +1,19 @@
+
 var sellid=document.getElementById("table");
-var sell=table.getElementsByTagName("td");//table内のtd要素
 var head=document.getElementById("head");
 
 
 sellid.addEventListener("click", function(e){
     var eventID=event.target.id;
     console.log(eventID);
+
     if(eventID==="0-0"||eventID=="0-1"||eventID=="0-2"||eventID=="0-3"||eventID=="0-4"||eventID=="0-5"||eventID=="0-6"||eventID=="1-0"||eventID=="2-0"||eventID=="3-0"||eventID=="4-0"||eventID=="5-0"||eventID=="6-0"||eventID=="7-0"){
  window.alert("選択しなおしてください");
 
 
-}else if(eventID[3]=="p"){
+}
+
+else if(eventID[3]=="p"){
     console.log(eventID[3]);
     
     var sl=e.target;
@@ -20,11 +23,17 @@ sellid.addEventListener("click", function(e){
 }
 
 else{
+var sl=e.terget;
+sl=window.prompt("講義名を入力");
+
+if(sl!=""&&sl!=null){
    
 console.log(e.target);
-
-var sl=e.target;
-sl=window.prompt("講義名を入力");
-console.log(sl);
 e.target.innerHTML=sl;
-}});
+    
+}
+else{
+    window.alert("入力がキャンセルされました");
+}
+}
+});
