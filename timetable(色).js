6c4f7dfc78bfd45047e4b07ac69d14803b13661c
kiyoto328 <kiyoto.a328@docomo.ne.jp>
2019-07-17 17:42:15 +0900

    月１限～火１限まではページ飛べるよ

---
+++ timetable(色).js
@@ -0,0 +1,450 @@
+var a_eventID;
+var l;
+var a=document.getElementsByClassName('a');
+for(let i=0;i<a.length;i++){
+  a[i].addEventListener('click',function(e){
+    a_eventID=event.target.id;
+    console.log(i);
+    console.log(a_eventID);
+    SelectPage(i);
+  })}
+  
+
+
+
+function SelectPage( page ) {
+l=page;
+  /*
+      各ページのエレメントを取得
+  */
+  var elementPage0 = document.getElementById( "page0" );
+  var elementPage1 = document.getElementById( "page1" );
+  var elementPage2 = document.getElementById( "page2" );
+  var elementPage3 = document.getElementById( "page3" );
+  var elementPage4 = document.getElementById( "page4" );
+  var elementPage5 = document.getElementById( "page5" );
+  var elementPage6 = document.getElementById( "page6" );
+  var elementPage7 = document.getElementById( "page7" );
+  var elementPage8 = document.getElementById( "page8" );
+  var elementPage9 = document.getElementById( "page9" );
+  var elementPage10 = document.getElementById( "page10" );
+  var elementPage11 = document.getElementById( "page11" );
+  var elementPage12 = document.getElementById( "page12" );
+  var elementPage13 = document.getElementById( "page13" );
+  var elementPage14 = document.getElementById( "page14" );
+  var elementPage15 = document.getElementById( "page15" );
+  var elementPage16 = document.getElementById( "page16" );
+  var elementPage17 = document.getElementById( "page17" );
+  var elementPage18 = document.getElementById( "page18" );
+  var elementPage19= document.getElementById( "page19" );
+  var elementPage20 = document.getElementById( "page20" );
+  var elementPage21 = document.getElementById( "page21" );
+  var elementPage22 = document.getElementById( "page22" );
+  var elementPage23 = document.getElementById( "page23" );
+  var elementPage24 = document.getElementById( "page24" );
+  var elementPage25 = document.getElementById( "page25" );
+  var elementPage26 = document.getElementById( "page26" );
+  var elementPage27 = document.getElementById( "page27" );
+  var elementPage28 = document.getElementById( "page28" );
+  var elementPage29 = document.getElementById( "page29" );
+  var elementPage30 = document.getElementById( "page30" );
+  var elementPage31 = document.getElementById( "page31" );
+  var elementPage32 = document.getElementById( "page32" );
+  var elementPage33 = document.getElementById( "page33" );
+  var elementPage34 = document.getElementById( "page34" );
+  var elementPage35 = document.getElementById( "page35" );
+  var elementPage36 = document.getElementById( "page36" );
+  var elementPage37 = document.getElementById( "page37" );
+  var elementPage38 = document.getElementById( "page38" );
+  var elementPage39 = document.getElementById( "page39" );
+  var elementPage40 = document.getElementById( "page40" );
+  var elementPage41 = document.getElementById( "page41" );
+
+  switch( page ) {
+  case 0:
+    elementPage0.style.display = 'block';
+    elementPage1.style.display = 'none';
+    elementPage2.style.display = 'none';
+    elementPage3.style.display = 'none';
+    elementPage4.style.display = 'none';
+    elementPage5.style.display = 'none';
+    elementPage6.style.display = 'none';
+    elementPage7.style.display = 'none';
+    elementPage8.style.display = 'none';
+    elementPage9.style.display = 'none';
+    elementPage10.style.display = 'none';
+    elementPage11.style.display = 'none';
+    elementPage12.style.display = 'none';
+    elementPage13.style.display = 'none';
+    elementPage14.style.display = 'none';
+    elementPage15.style.display = 'none';
+    elementPage16.style.display = 'none';
+    elementPage17.style.display = 'none';
+    elementPage18.style.display = 'none';
+    elementPage19.style.display = 'none';
+    elementPage20.style.display = 'none';
+    elementPage21.style.display = 'none';
+    elementPage22.style.display = 'none';
+    elementPage23.style.display = 'none';
+    elementPage24.style.display = 'none';
+    elementPage25.style.display = 'none';
+    elementPage26.style.display = 'none';
+    elementPage27.style.display = 'none';
+    elementPage28.style.display = 'none';
+    elementPage29.style.display = 'none';
+    elementPage30.style.display = 'none';
+    elementPage31.style.display = 'none';
+    elementPage32.style.display = 'none';
+    elementPage33.style.display = 'none';
+    elementPage34.style.display = 'none';
+    elementPage35.style.display = 'none';
+    elementPage36.style.display = 'none';
+    elementPage37.style.display = 'none';
+    elementPage38.style.display = 'none';
+    elementPage39.style.display = 'none';
+    elementPage40.style.display = 'none';
+    elementPage41.style.display = 'none';
+     
+      
+      break;
+
+  case 1:
+      
+      elementPage0.style.display = 'none';
+      elementPage1.style.display = 'block';
+      elementPage2.style.display = 'none';
+      elementPage3.style.display = 'none';
+      elementPage4.style.display = 'none';
+      elementPage5.style.display = 'none';
+      elementPage6.style.display = 'none';
+      elementPage7.style.display = 'none';
+      elementPage8.style.display = 'none';
+      elementPage9.style.display = 'none';
+      elementPage10.style.display = 'none';
+      elementPage11.style.display = 'none';
+      elementPage12.style.display = 'none';
+      elementPage13.style.display = 'none';
+      elementPage14.style.display = 'none';
+      elementPage15.style.display = 'none';
+      elementPage16.style.display = 'none';
+      elementPage17.style.display = 'none';
+      elementPage18.style.display = 'none';
+      elementPage19.style.display = 'none';
+      elementPage20.style.display = 'none';
+      elementPage21.style.display = 'none';
+      elementPage22.style.display = 'none';
+      elementPage23.style.display = 'none';
+      elementPage24.style.display = 'none';
+      elementPage25.style.display = 'none';
+      elementPage26.style.display = 'none';
+      elementPage27.style.display = 'none';
+      elementPage28.style.display = 'none';
+      elementPage29.style.display = 'none';
+      elementPage30.style.display = 'none';
+      elementPage31.style.display = 'none';
+      elementPage32.style.display = 'none';
+      elementPage33.style.display = 'none';
+      elementPage34.style.display = 'none';
+      elementPage35.style.display = 'none';
+      elementPage36.style.display = 'none';
+      elementPage37.style.display = 'none';
+      elementPage38.style.display = 'none';
+      elementPage39.style.display = 'none';
+      elementPage40.style.display = 'none';
+      elementPage41.style.display = 'none';
+
+      break;
+
+  case 2:
+    elementPage0.style.display = 'none';
+    elementPage1.style.display = 'none';
+    elementPage2.style.display = 'block';
+    elementPage3.style.display = 'none';
+    elementPage4.style.display = 'none';
+    elementPage5.style.display = 'none';
+    elementPage6.style.display = 'none';
+    elementPage7.style.display = 'none';
+    elementPage8.style.display = 'none';
+    elementPage9.style.display = 'none';
+    elementPage10.style.display = 'none';
+    elementPage11.style.display = 'none';
+    elementPage12.style.display = 'none';
+    elementPage13.style.display = 'none';
+    elementPage14.style.display = 'none';
+    elementPage15.style.display = 'none';
+    elementPage16.style.display = 'none';
+    elementPage17.style.display = 'none';
+    elementPage18.style.display = 'none';
+    elementPage19.style.display = 'none';
+    elementPage20.style.display = 'none';
+    elementPage21.style.display = 'none';
+    elementPage22.style.display = 'none';
+    elementPage23.style.display = 'none';
+    elementPage24.style.display = 'none';
+    elementPage25.style.display = 'none';
+    elementPage26.style.display = 'none';
+    elementPage27.style.display = 'none';
+    elementPage28.style.display = 'none';
+    elementPage29.style.display = 'none';
+    elementPage30.style.display = 'none';
+    elementPage31.style.display = 'none';
+    elementPage32.style.display = 'none';
+    elementPage33.style.display = 'none';
+    elementPage34.style.display = 'none';
+    elementPage35.style.display = 'none';
+    elementPage36.style.display = 'none';
+    elementPage37.style.display = 'none';
+    elementPage38.style.display = 'none';
+    elementPage39.style.display = 'none';
+    elementPage40.style.display = 'none';
+    elementPage41.style.display = 'none';
+  
+      break;
+      case 3:
+          elementPage0.style.display = 'none';
+          elementPage1.style.display = 'none';
+          elementPage2.style.display = 'none';
+          elementPage3.style.display = 'block';
+          elementPage4.style.display = 'none';
+          elementPage5.style.display = 'none';
+          elementPage6.style.display = 'none';
+          elementPage7.style.display = 'none';
+          elementPage8.style.display = 'none';
+          elementPage9.style.display = 'none';
+          elementPage10.style.display = 'none';
+          elementPage11.style.display = 'none';
+          elementPage12.style.display = 'none';
+          elementPage13.style.display = 'none';
+          elementPage14.style.display = 'none';
+          elementPage15.style.display = 'none';
+          elementPage16.style.display = 'none';
+          elementPage17.style.display = 'none';
+          elementPage18.style.display = 'none';
+          elementPage19.style.display = 'none';
+          elementPage20.style.display = 'none';
+          elementPage21.style.display = 'none';
+          elementPage22.style.display = 'none';
+          elementPage23.style.display = 'none';
+          elementPage24.style.display = 'none';
+          elementPage25.style.display = 'none';
+          elementPage26.style.display = 'none';
+          elementPage27.style.display = 'none';
+          elementPage28.style.display = 'none';
+          elementPage29.style.display = 'none';
+          elementPage30.style.display = 'none';
+          elementPage31.style.display = 'none';
+          elementPage32.style.display = 'none';
+          elementPage33.style.display = 'none';
+          elementPage34.style.display = 'none';
+          elementPage35.style.display = 'none';
+          elementPage36.style.display = 'none';
+          elementPage37.style.display = 'none';
+          elementPage38.style.display = 'none';
+          elementPage39.style.display = 'none';
+          elementPage40.style.display = 'none';
+          elementPage41.style.display = 'none';
+  break;
+  case 4:
+      elementPage0.style.display = 'none';
+      elementPage1.style.display = 'none';
+      elementPage2.style.display = 'none';
+      elementPage3.style.display = 'none';
+      elementPage4.style.display = 'block';
+      elementPage5.style.display = 'none';
+      elementPage6.style.display = 'none';
+      elementPage7.style.display = 'none';
+      elementPage8.style.display = 'none';
+      elementPage9.style.display = 'none';
+      elementPage10.style.display = 'none';
+      elementPage11.style.display = 'none';
+      elementPage12.style.display = 'none';
+      elementPage13.style.display = 'none';
+      elementPage14.style.display = 'none';
+      elementPage15.style.display = 'none';
+      elementPage16.style.display = 'none';
+      elementPage17.style.display = 'none';
+      elementPage18.style.display = 'none';
+      elementPage19.style.display = 'none';
+      elementPage20.style.display = 'none';
+      elementPage21.style.display = 'none';
+      elementPage22.style.display = 'none';
+      elementPage23.style.display = 'none';
+      elementPage24.style.display = 'none';
+      elementPage25.style.display = 'none';
+      elementPage26.style.display = 'none';
+      elementPage27.style.display = 'none';
+      elementPage28.style.display = 'none';
+      elementPage29.style.display = 'none';
+      elementPage30.style.display = 'none';
+      elementPage31.style.display = 'none';
+      elementPage32.style.display = 'none';
+      elementPage33.style.display = 'none';
+      elementPage34.style.display = 'none';
+      elementPage35.style.display = 'none';
+      elementPage36.style.display = 'none';
+      elementPage37.style.display = 'none';
+      elementPage38.style.display = 'none';
+      elementPage39.style.display = 'none';
+      elementPage40.style.display = 'none';
+      elementPage41.style.display = 'none';
+    break;
+    case 5:
+    elementPage0.style.display = 'none';
+    elementPage1.style.display = 'none';
+    elementPage2.style.display = 'none';
+    elementPage3.style.display = 'none';
+    elementPage4.style.display = 'none';
+    elementPage5.style.display = 'block';
+    elementPage6.style.display = 'none';
+    elementPage7.style.display = 'none';
+    elementPage8.style.display = 'none';
+    elementPage9.style.display = 'none';
+    elementPage10.style.display = 'none';
+    elementPage11.style.display = 'none';
+    elementPage12.style.display = 'none';
+    elementPage13.style.display = 'none';
+    elementPage14.style.display = 'none';
+    elementPage15.style.display = 'none';
+    elementPage16.style.display = 'none';
+    elementPage17.style.display = 'none';
+    elementPage18.style.display = 'none';
+    elementPage19.style.display = 'none';
+    elementPage20.style.display = 'none';
+    elementPage21.style.display = 'none';
+    elementPage22.style.display = 'none';
+    elementPage23.style.display = 'none';
+    elementPage24.style.display = 'none';
+    elementPage25.style.display = 'none';
+    elementPage26.style.display = 'none';
+    elementPage27.style.display = 'none';
+    elementPage28.style.display = 'none';
+    elementPage29.style.display = 'none';
+    elementPage30.style.display = 'none';
+    elementPage31.style.display = 'none';
+    elementPage32.style.display = 'none';
+    elementPage33.style.display = 'none';
+    elementPage34.style.display = 'none';
+    elementPage35.style.display = 'none';
+    elementPage36.style.display = 'none';
+    elementPage37.style.display = 'none';
+    elementPage38.style.display = 'none';
+    elementPage39.style.display = 'none';
+    elementPage40.style.display = 'none';
+    elementPage41.style.display = 'none';
+    break;
+    case 6:
+        elementPage0.style.display = 'none';
+        elementPage1.style.display = 'none';
+        elementPage2.style.display = 'none';
+        elementPage3.style.display = 'none';
+        elementPage4.style.display = 'none';
+        elementPage5.style.display = 'none';
+        elementPage6.style.display = 'block';
+        elementPage7.style.display = 'none';
+        elementPage8.style.display = 'none';
+        elementPage9.style.display = 'none';
+        elementPage10.style.display = 'none';
+        elementPage11.style.display = 'none';
+        elementPage12.style.display = 'none';
+        elementPage13.style.display = 'none';
+        elementPage14.style.display = 'none';
+        elementPage15.style.display = 'none';
+        elementPage16.style.display = 'none';
+        elementPage17.style.display = 'none';
+        elementPage18.style.display = 'none';
+        elementPage19.style.display = 'none';
+        elementPage20.style.display = 'none';
+        elementPage21.style.display = 'none';
+        elementPage22.style.display = 'none';
+        elementPage23.style.display = 'none';
+        elementPage24.style.display = 'none';
+        elementPage25.style.display = 'none';
+        elementPage26.style.display = 'none';
+        elementPage27.style.display = 'none';
+        elementPage28.style.display = 'none';
+        elementPage29.style.display = 'none';
+        elementPage30.style.display = 'none';
+        elementPage31.style.display = 'none';
+        elementPage32.style.display = 'none';
+        elementPage33.style.display = 'none';
+        elementPage34.style.display = 'none';
+        elementPage35.style.display = 'none';
+        elementPage36.style.display = 'none';
+        elementPage37.style.display = 'none';
+        elementPage38.style.display = 'none';
+        elementPage39.style.display = 'none';
+        elementPage40.style.display = 'none';
+        elementPage41.style.display = 'none';
+        break;
+  default:
+    elementPage0.style.display = 'none';
+    elementPage1.style.display = 'none';
+    elementPage2.style.display = 'none';
+    elementPage3.style.display = 'none';
+    elementPage4.style.display = 'none';
+    elementPage5.style.display = 'none';
+    elementPage6.style.display = 'none';
+    elementPage7.style.display = 'none';
+    elementPage8.style.display = 'none';
+    elementPage9.style.display = 'none';
+    elementPage10.style.display = 'none';
+    elementPage11.style.display = 'none';
+    elementPage12.style.display = 'none';
+    elementPage13.style.display = 'none';
+    elementPage14.style.display = 'none';
+    elementPage15.style.display = 'none';
+    elementPage16.style.display = 'none';
+    elementPage17.style.display = 'none';
+    elementPage18.style.display = 'none';
+    elementPage19.style.display = 'none';
+    elementPage20.style.display = 'none';
+    elementPage21.style.display = 'none';
+    elementPage22.style.display = 'none';
+    elementPage23.style.display = 'none';
+    elementPage24.style.display = 'none';
+    elementPage25.style.display = 'none';
+    elementPage26.style.display = 'none';
+    elementPage27.style.display = 'none';
+    elementPage28.style.display = 'none';
+    elementPage29.style.display = 'none';
+    elementPage30.style.display = 'none';
+    elementPage31.style.display = 'none';
+    elementPage32.style.display = 'none';
+    elementPage33.style.display = 'none';
+    elementPage34.style.display = 'none';
+    elementPage35.style.display = 'none';
+    elementPage36.style.display = 'none';
+    elementPage37.style.display = 'none';
+    elementPage38.style.display = 'none';
+    elementPage39.style.display = 'none';
+    elementPage40.style.display = 'none';
+    elementPage41.style.display = 'none';
+
+      break;
+  }
+};
+
+/*var a_eventID;
+           for(let i=0;i<a.length;i++){
+               a[i].addEventListener('click',function(e){
+                 a_eventID=event.target.id;
+                 
+
+
+               })
+           }
+
+*/
+
+
+
+function changesytle(cssid,cssfile) {
+    document.getElementById(cssid).href = cssfile;
+ }//色を変えるための関数です
+
+ function change_page(){
+   console.log(a[l]);
+  a[l].style.color='#ff0000';
+ }
+ 
+ 
