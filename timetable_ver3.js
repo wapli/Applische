ea756ed4f1426a6157458db7187f8a22f4df0f1e
kinntaro <moneybook0409@outlook.jp>
2019-12-12 18:34:22 +0900

    no message

--- timetable_ver3.js
+++ timetable_ver3.js
@@ -1,100 +1,73 @@
 var finishid=document.getElementById("finish");
 var editid=document.getElementById("edit");
 const sell=document.getElementsByClassName('sell');
-  var sella;
 
+  
+  
 function changesytle(cssid,cssfile) {
-    document.getElementById(cssid).href = cssfile;
- }
-
-
- editid.addEventListener("click",input_edit);
-finishid.addEventListener("click",input_enter);
-
-   
-
+      document.getElementById(cssid).href = cssfile;
+   }
+  
+  
+   editid.addEventListener("click",input_edit);//入力ボタン
+  finishid.addEventListener("click",input_enter);//完了ボタン
+  
+     
+  //入力
 function input_edit(){
-           
-    for(let i = 0; i < sell.length; i++){
-         edit(sell[i]);
-     }
+             
+      for(let i = 0; i < sell.length; i++){
+           edit(sell[i]);
+       }
+                        }
+  
+function input_enter(){
+          
+   for(let i = 0; i < sell.length; i++){
+        
+      enter(sell[i]);
     }
-
-    function input_enter(){
-        
- for(let i = 0; i < sell.length; i++){
-      
-    enter(sell[i]);
-  }
- }
- 
-
+                    }
+   
+  
 function enter(sella){
-    sella.removeEventListener("click",bbb);
-}
+      sella.removeEventListener("click",bbb);
+                    }
+  
+  
+function edit(sella){
+  
+           sella.addEventListener('click', bbb);
+  
+                    } 
 
 
- function edit(sella){
-
-         sella.addEventListener('click', bbb);
-
-}
-
-
- var bbb=function (){
+var bbb=function (){
      
-    console.log(sella);
-
     var eventID=event.target.id;
-    console.log(eventID);
-      if(eventID[3]=="p"){
-     
-         console.log(eventID[3]);
-     
-         
-     
-         var sl=this.target;
-     
-         sl=window.prompt("教室名を入力");
-     
-         this.target.innerHTML=sl;
-     
-     
-     
-     }
-     
-     
-     
-     else{
-     
-     var sl=this.terget;
-     
-     sl=window.prompt("講義名を入力");
-     
-     
-     
-     if(sl!=""&&sl!=null){
-     
-        
-     
-     console.log(this.target);
-     
-     this.target.innerHTML=sl;
-     
-         
-     
-     }
-     
-     else{
-     
-         window.alert("入力がキャンセルされました");
-     
-     }
-     
-     }
+      console.log(eventID);
+if(eventID[3]=="p"){   
+           sl=window.prompt("教室名を入力");      
+           event.target.innerHTML=sl;       
+                    }      
+       
+else{      
+       var sl=event.target;
+       sl=window.prompt("講義名を入力");   
 
- }
- 
+       if(sl!=""&&sl!=null){      
+       console.log(event.target);      
+       event.target.innerHTML=sl;
+                            }
+       
+       else{      
+           window.alert("入力がキャンセルされました");     
+           }      
+    }
+  
+                    }
+
+
  function a(num){
    
    
@@ -102,7 +75,7 @@ function enter(sella){
           if(num==0){
              document.getElementById("6").style.visibility="visible"; 
           
-       }
+                    }
        
        //6限非表示
           else if(num==1){
@@ -118,12 +91,12 @@ function enter(sella){
               document.getElementById("7").style.visibility="collapse"; 
           }
       
-     else if(num==4){
+          else if(num==4){
         
         document.getElementById("retu").style.visibility="collapse";
 
-     }
-     else{
+          }
+          else{
          document.getElementById("retu").style.visibility="visible";
-     }
-    }
+          }
+             }
