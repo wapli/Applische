771c291d275ff96963c2cd4bc880092becb0e88b
薗部匡矢 <masaya.tanks07@gmail.com>
2019-12-27 16:16:19 +0900

    zoom.jsの仕様を変更しました。機能自体は大きく変わっていませんが、onclickの実装をaddEventListenerの実装に変更しました。

--- akazawa12-23/js/zoom.js
+++ akazawa12-23/js/zoom.js
@@ -1,47 +1,44 @@
-//ver1.0
+//ver1.1 onclickの実装をaddEventListenerの実装に変更しました.
+
 document.addEventListener('init', (event) => {
     var page =event.target;
-    
     //マップ機能ページを読み込んだ場合
     if(page.id = "ons-map"){
-        var map = document.getElementById('map');
+        console.log("Loaded zoom.js");
+        var map = document.getElementById("mapImage");
         map;
-    }
 
+        //拡大倍率
+        var ratio=1;
+
+        //拡大機能
+        document.getElementById("zoomin").addEventListener("click", function(){
+            ratio *= 1.1;
+            console.log("Increased ratio:"+ratio);
+
+            map.style.width=String(ratio*100)+"vw";
+            map.style.height=String(ratio*100)+"vh";
+
+            if(ratio>2.5){
+                ratio=2.5;
+                map.style.width=String(ratio*100)+"vw";
+                map.style.height=String(ratio*100)+"vh";
+            }
+        }, false);
+
+        //縮小機能
+        document.getElementById("zoomout").addEventListener("click", function(){
+            ratio *= 0.9;
+            console.log("Decreased ratio:"+ratio);
+
+            map.style.width=String(ratio*100)+"vw";
+            map.style.height=String(ratio*100)+"vh";
+
+            if(ratio<1){
+                ratio=1;
+                map.style.width=String(ratio*100)+"vw";
+                map.style.height=String(ratio*100)+"vh";
+            }
+        }, false);
+    }
 },false);
-
-//拡大倍率
-var ratio=1;
-
-//拡大関数
-function zoomIn(){
-    ratio *= 1.1;
-
-    map.style.width=String(ratio*100)+"vw";
-    map.style.height=String(ratio*100)+"vh";
-
-    if(map.width<window.innerWidth || map.height<window.innerHeight){
-        ratio=1;
-    }
-    else if(map.width>window.innerWidth*2 || map.height>window.innerHeight*2){
-        map.style.width=String(200)+"vw";
-        map.style.height=String(200)+"vh";
-    }
-}
-
-//縮小関数
-function zoomOut(){
-    ratio *= 0.9;
-
-    map.style.width=String(ratio*100)+"vw";
-    map.style.height=String(ratio*100)+"vh";
-
-
-    if(map.width<window.innerWidth || map.height<window.innerHeight){
-        ratio=1;
-    }
-    else if(map.width>window.innerWidth*2 || map.height>window.innerHeight*2){
-        map.style.width=String(200)+"vw";
-        map.style.height=String(200)+"vh";
-    }
-}
