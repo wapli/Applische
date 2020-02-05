dad903e324bd39909440faed730ea824e4f688ed
Be3 <49334354+Be3751@users.noreply.github.com>
2020-02-05 14:43:11 +0900

    機能を追加しました。
    追加内容：
    デバイスがandroidの場合、マップ画像の幅をデバイスの画面サイズに合わせる。

--- app2月4日/js/distinguishDevice.js
+++ app2月4日/js/distinguishDevice.js
@@ -3,11 +3,15 @@ document.addEventListener("init", function(event){
 
     if(page.id="ons-map"){
         var ua = navigator.userAgent;
+        alert(ua);
         if(ua.indexOf('iPhone') > 0){
             alert("iPhone");
         }
         else if(ua.indexOf('Android') > 0){
             alert("Android");
+            var map = document.getElementById("mapImage");
+            map.style.width = window.innerWidth;
+            map.style.height = window.innerHeight;
         }
     }
 
