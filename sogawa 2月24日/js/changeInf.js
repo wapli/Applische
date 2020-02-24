575bf78c7387dd4b1bdad643839caa575d42cea3
薗部匡矢 <masaya.tanks07@gmail.com>
2020-02-24 15:27:22 +0900

    各種マップ用jsからinitイベント用のコードを除去しました。

--- sogawa 2月24日/js/changeInf.js
+++ sogawa 2月24日/js/changeInf.js
@@ -1,36 +1,34 @@
 // 各種情報を書き換えるためのjs
-document.addEventListener("init", function(event){
-    var lib_time = document.getElementById("lib_time");
-    var date = new Date();
-    var dayOfWeek = date.getDay();
-    switch(dayOfWeek){
-        //日曜
-        case 0:
-            lib_time.innerHTML = "10:00~17:00";
-            break;
-        //月曜
-        case 1:
-            lib_time.innerHTML = "9:00~21:00";
-            break;
-        //火曜
-        case 2:
-            lib_time.innerHTML = "9:00~21:00";
-            break;
-        //水曜
-        case 3:
-            lib_time.innerHTML = "9:00~21:00";
-            break;
-        //木曜
-        case 4:
-            lib_time.innerHTML = "9:00~21:00";
-            break;
-        //金曜
-        case 5:
-            lib_time.innerHTML = "9:00~21:00";
-            break;
-        //土曜
-        case 6:
-            lib_time.innerHTML = "9:00~17:00";
-            break;
-    }
-}, false);
+var lib_time = document.getElementById("lib_time");
+var date = new Date();
+var dayOfWeek = date.getDay();
+switch(dayOfWeek){
+    //日曜
+    case 0:
+        lib_time.innerHTML = "10:00~17:00";
+        break;
+    //月曜
+    case 1:
+        lib_time.innerHTML = "9:00~21:00";
+        break;
+    //火曜
+    case 2:
+        lib_time.innerHTML = "9:00~21:00";
+        break;
+    //水曜
+    case 3:
+        lib_time.innerHTML = "9:00~21:00";
+        break;
+    //木曜
+    case 4:
+        lib_time.innerHTML = "9:00~21:00";
+        break;
+    //金曜
+    case 5:
+        lib_time.innerHTML = "9:00~21:00";
+        break;
+    //土曜
+    case 6:
+        lib_time.innerHTML = "9:00~17:00";
+        break;
+}
--- sogawa 2月24日/js/openInf.js
+++ sogawa 2月24日/js/openInf.js
@@ -1,31 +1,24 @@
 //ver1.1 マップ機能内で建物情報を提示する機能
 
-//建物の集合と建物情報の集合をそれぞれ取得
-var objs = document.getElementsByClassName("obj");
-var objInfs = document.getElementsByClassName("obj_inf");
+// //建物の集合と建物情報の集合をそれぞれ取得
+// var objs = document.getElementsByClassName("obj");
+// var objInfs = document.getElementsByClassName("obj_inf");
 
-document.addEventListener('init', function(event){
-    var page = event.target;
-    if(page.id == "ons-map"){
-        console.log("Loaded zoom.js");
-        for(let i=0;i<objs.length;i++){
-            objs[i].addEventListener("click", function(){
-                console.log("obj"+i+" was clicked.");
+// console.log("Loaded openInf.js");
+// for(let i=0;i<objs.length;i++){
+//     objs[i].addEventListener("click", function(){
+//         console.log("obj"+i+" was clicked.");
 
-                //拡大倍率ratioが2倍以上の場合のみ、吹き出しを表示
-                if(ratio>=2){
-                    console.log("possible to open information.");
-                    objInfs[i].style.display="block";
-                }
-                else{
-                    console.log("not possible to open information.");
-                }
-            }, false);
-        }
-            
-        
-    }
-},false);
+//         //拡大倍率ratioが2倍以上の場合のみ、吹き出しを表示
+//         if(ratio>=2){
+//             console.log("possible to open information.");
+//             objInfs[i].style.display="block";
+//         }
+//         else{
+//             console.log("not possible to open information.");
+//         }
+//     }, false);
+// }
 
 //addEventListenerが効かない場合に備えて関数
 function openInf(num)
